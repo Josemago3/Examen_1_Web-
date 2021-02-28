a89d9c72b04a945adb296c9ccb2a586d4a54ea5e
Jose <josemago3@hotmail.com>
2021-02-28 10:14:54 -0600

    Se agregaron los logos y la funciones
    la función cobrar es para el botón de cobrar, función suma, función salir, solo falta la función, agregar, editar, eleminar

--- script.js
+++ script.js
@@ -8,13 +8,15 @@ var comida= [{nombre: "Coca-Cola", descripcion : "Bebida sabor cola de 300ml", p
              {nombre: "Memela", descripcion : "Tortilla de maíz elaborada a mano con salsa y quesillo", precio : "30", tipo : "Platillo"},
              {nombre: "Flan", descripcion : "Flan sabor vainilla con cubierta de caramelo", precio : "30", tipo : "Postre"},
              {nombre: "Nieve de Limón", descripcion : "Nieve sabor limón elaborada artesanalmente en el estado de puebla", precio : "15", tipo : "Postre"},
-             {nombre: "Gelatina", descripcion : "Gelatina sabor rompope", precio : "10", tipo : "Postre"}]; 
+             {nombre: "Gelatina", descripcion : "Gelatina sabor rompope", precio : "10.50", tipo : "Postre"}]; 
+var ventas=0;
 
 function mostrarBebidas() {
     var j=1;
     ocultarImagen();
     document.getElementById("comida").style.display = "none";
     document.getElementById("postre").style.display = "none";
+    document.getElementById("editar").style.display = "none";
     document.getElementById("bebidas").style.display = "block";
     
     for(i=0;i<comida.length;i++){
@@ -35,6 +37,7 @@ function mostrarComida() {
     ocultarImagen();
     document.getElementById("bebidas").style.display = "none";
     document.getElementById("postre").style.display = "none";
+    document.getElementById("editar").style.display = "none";
     document.getElementById("comida").style.display = "block";
 
     for(i=0;i<comida.length;i++){
@@ -55,6 +58,7 @@ function mostrarPost() {
     ocultarImagen();
     document.getElementById("bebidas").style.display = "none";
     document.getElementById("comida").style.display = "none";
+    document.getElementById("editar").style.display = "none";
     document.getElementById("postre").style.display = "block";
 
     for(i=0;i<comida.length;i++){
@@ -68,6 +72,114 @@ function mostrarPost() {
             j++;
         }
     }
+}
+
+function editar(){
+    var j=0;
+    ocultarImagen();
+    document.getElementById("bebidas").style.display = "none";
+    document.getElementById("comida").style.display = "none";
+    document.getElementById("postre").style.display = "none";
+    document.getElementById("editar").style.display = "block";
+
+    for(i=0;i<comida.length;i++){
+        var producto= "producto"+(i+1);
+        var descripcion= "descripción"+(i+1);
+        var precio= "precio"+(i+1);
+        var tipo= "tipo"+(i+1);
+        document.getElementById(producto).innerHTML = comida[i].nombre;
+        document.getElementById(descripcion).innerHTML = comida[i].descripcion;
+        document.getElementById(precio).innerHTML = comida[i].precio;
+        document.getElementById(tipo).innerHTML = comida[i].tipo;
+    }
+
+    //Agregar
+    //document.getElementById("Agregar");
+    //Eliminar
+}
+
+function agregar(){
+
+}
+
+function edit(){
+    carros.filter(carro => carro.color === "rojo");
+}
+
+function eliminar(){
+
+}
+
+function suma(){
+    var suma=0.00,B=0,P=0,O=0;
+    for(i=0;i<4;i++){
+        if(B<3){
+            var precio= "bprecio"+(P+1);
+            var cantidad = "bT"+(P+1);
+            var n =document.getElementById(precio).innerHTML;
+            console.log(n);
+            console.log(document.getElementById(cantidad).value);
+            suma+= document.getElementById(precio).innerHTML * document.getElementById(cantidad).value;
+            B++;
+        }
+        if(P<4){
+            var precio= "Pprecio"+(P+1);
+            var cantidad = "PT"+(P+1);
+            var n =document.getElementById(precio).innerHTML;
+            console.log(n);
+            console.log(document.getElementById(cantidad).value);
+            suma+= document.getElementById(precio).innerHTML * document.getElementById(cantidad).value;
+            P++;
+        }
+        if(O<3){
+            var precio= "oprecio"+(O+1);
+            var cantidad = "oT"+(O+1);
+            var n =document.getElementById(precio).innerHTML;
+            console.log(n);
+            console.log(document.getElementById(cantidad).value);
+            suma+= document.getElementById(precio).innerHTML * document.getElementById(cantidad).value;
+            O++;
+        }
+    }
+    if(suma==0){
+        document.getElementById("Total").value = "0.00";
+    }
+    else{
+        document.getElementById("Total").value = suma;
+    }
+}
+
+function comprar(){
+    var B=0,P=0,O=0;
+
+    suma();
+
+    for(i=0;i<4;i++){
+        if(B<3){
+            var cantidad = "bT"+(P+1);
+            document.getElementById(cantidad).value = 0;
+            B++;
+        }
+        if(P<4){
+            var cantidad = "PT"+(P+1);
+            document.getElementById(cantidad).value = 0;
+            P++;
+        }
+        if(O<3){
+            var cantidad = "oT"+(O+1);
+            document.getElementById(cantidad).value = 0;
+            O++;
+        }
+    }
+
+    ventas+= parseFloat(document.getElementById("Total").value);
+    alert("¡Compra realizada exitosamente! \nTotal de la comprar: "+ document.getElementById("Total").value);
+    document.getElementById("Total").value = "0.00";
+}
+
+function salir(){
+    alert("Ganancias Totales: "+ventas);
+    location.reload();
 }
 
 function ocultarImagen(){
